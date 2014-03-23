0775185a7b575751cfe1c76aaef85eb02d35bf45
Sam Dods <sam@theappbusiness.com>
2014-03-23 12:23:41 +0000

    Initial Commit

---
+++ DZLPlistObject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DZLPlistObject
+//
+//  Created by Sam Dods on 23/03/2014.
+//  Copyright (c) 2014 Sam Dods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
